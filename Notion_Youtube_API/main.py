a6950e96ad0d532dcba006b9eb4c89b9d34677de
Ivan Gulyaev <gulivan@yandex.ru>
2021-06-08 02:37:47 +0300

    Working on add_property method

--- Notion_Youtube_API/main.py
+++ Notion_Youtube_API/main.py
@@ -1,5 +1,6 @@
 import os
 import json
+from typing import Union, Dict
 import requests
 from dotenv import load_dotenv
 load_dotenv()
@@ -18,17 +19,43 @@ class NotionData:
         self.data["parent"] = {"database_id": target_db}
         self.headers["Authorization"] = f"Bearer {notion_secret}"
 
-    def add_property(self, field_name: str, value: str, property_type: str = 'title'):
-        if field_name in self.data["properties"]:
+    def add_property(self,
+                     field_name: str,
+                     value: Union[str, Dict[str, str]],
+                     property_type: str = 'title',
+                     overwrite: bool = False):
+        """
+        Adds a new data property to the row
+        :param str field_name: Field name in the table
+        :param str value: Field value
+        :param str property_type: Field type.
+        For more: https://developers.notion.com/reference/database#database-properties
+        :param bool overwrite: If True - overwrites value with the same name
+        """
+        if field_name in self.data["properties"] and not overwrite:
             raise ValueError(
-                f"A property named {field_name} already exists in this new entry."
+                f"A property named {field_name} already exists in this entry."
             )
-        self.data["properties"][field_name] = {f"{property_type}": [{"text": {"content": f"{value}"}}]}
+        if property_type == 'title':
+            self.data["properties"][field_name] = {property_type: [{"text": {"content": value}}]}
+        elif property_type == 'url':
+            self.data["properties"][field_name] = {property_type: value}
+        elif property_type == 'select':
+            self.data["properties"][field_name] = {property_type: value}
 
-    def publish_row(self):
+    def publish_row(self, print_curl=False):
         r = requests.post(url=NOTION_ENDPOINT,
                           data=json.dumps(self.data),
                           headers=self.headers)
+        if print_curl:
+            req = r.request
+            command = "curl -X {method} -H {headers} -d '{data}' '{uri}'"
+            method = req.method
+            uri = req.url
+            data = req.body
+            headers = ['"{0}: {1}"'.format(k, v) for k, v in req.headers.items()]
+            headers = " -H ".join(headers)
+            print(command.format(method=method, headers=headers, data=data, uri=uri))
         if r.status_code != 200:
             raise ValueError(
                 f"Error code: {r.json()['code']}. Message: {r.json()['message']}"
@@ -36,6 +63,17 @@ class NotionData:
         return True
 
 
-notion = NotionData(notion_secret=NOTION_SECRET, target_db=TARGET_DB)
-notion.add_property(field_name='Name', value='fsadfafds')
-notion.publish_row()
+notion = NotionData(notion_secret=NOTION_SECRET,
+                    target_db=TARGET_DB)
+notion.add_property(field_name='Title',
+                    property_type='title',
+                    value='Video title')
+notion.add_property(field_name='Link',
+                    property_type='url',
+                    value='https://ya.ru')
+notion.add_property(field_name='Playlist',
+                    property_type='select',
+                    value={"name": "non existing select"})
+notion.publish_row(print_curl=True)
+
+print(42)
